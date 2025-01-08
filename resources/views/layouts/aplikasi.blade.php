13c3a9a33ecc3f4e40c055c26d2ee0118af4ee5a
icha <ellaainurriska1@gmail.com>
2025-01-08 23:30:27 +0700

    menambah folder, membuat kode agar bisa memanggil ke file aplikasi dan mengubah roote

--- resources/views/layouts/aplikasi.blade.php
+++ resources/views/layouts/aplikasi.blade.php
@@ -29,24 +29,14 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Dashboard</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Widgets</li>
-            </ol>
-          </div>
-        </div>
+        @yield('header')
       </div><!-- /.container-fluid -->
     </section>
 
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
-        ichaaaaaa
+      @yield('content')
       </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
