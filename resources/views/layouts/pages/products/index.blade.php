031cc9a9b35951310c556f1d782fa2a968147591
icha <ellaainurriska1@gmail.com>
2025-01-11 21:26:58 +0700

    membuat tabel didalam index

--- resources/views/layouts/pages/products/index.blade.php
+++ resources/views/layouts/pages/products/index.blade.php
@@ -15,5 +15,36 @@
 @endsection
 
 @section('content')
-    ichaaaa
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  <th>no</th>
+                  <th>Produk</th>
+                  <th>Description</th>
+                  <th>Kode Barang</th>
+                  <th>Harga</th>
+                  <th>Stok</th>
+                  <th>Katagori</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                <td>no</td>
+                <td>Produk</td>
+                <td>Description</td>
+                <td>Kode Barang</td>
+                <td>Harga</td>
+                <td>Stok</td>
+                <td>Katagori</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
 @endsection
