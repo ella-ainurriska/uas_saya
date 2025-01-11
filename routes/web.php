7726defb451d4f7c8eb6c11eab548b478b011db3
icha <ellaainurriska1@gmail.com>
2025-01-11 20:03:53 +0700

    menggunak controller didalam route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\admin\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('layouts.pages.products.index');
-});
+// Route::get('/', function () {
+//     return view('layouts.pages.products.index');
+// });
+
+Route::get('/',[ProductController::class, 'index']);
